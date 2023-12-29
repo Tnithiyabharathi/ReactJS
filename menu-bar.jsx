d1a351cfbeb1146c139a36dd17a8071ff28bf1c2
Tnithiyabharathi <106990907+Tnithiyabharathi@users.noreply.github.com>
2023-12-29 20:14:10 +0530

    Add files via upload

--- menu-bar.jsx
+++ menu-bar.jsx
@@ -4,16 +4,18 @@ import "./menu-bar.css"
 export function MenuBar()
 {
     return(
-       <ul>
-        <li>
-            <div class="dropdown">
-                <a href="#sale">Sale</a>
-            </div>
-        </li>
-        <li><a href="#inventory">Inventory</a></li>
-        <li><a href="#customer">Customer</a></li>
-        <li><a href="#employee">Employee</a></li>
-        <li><a href="#bill">Bill</a></li>
-       </ul>
+    <div class="dropdown">
+        <ul>
+            <li><a href="#sale">Sale</a></li>
+            <li><a href="#inventory">Inventory</a></li>
+            <li><a href="#customer">Customer</a></li>
+            <li><a href="#employee">Employee</a></li>
+            <li><a href="#bill">Bill</a></li>
+        </ul>
+        <div class="dropdown-content">
+       <h5>Make Sale</h5>
+       <h5>Reports</h5>    
+        </div>
+    </div>  
     )
 } 
