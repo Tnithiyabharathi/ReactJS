05b2a817eabcc199ae21ef01206643c640080b04
Tnithiyabharathi <106990907+Tnithiyabharathi@users.noreply.github.com>
2024-01-04 19:05:45 +0530

    Add files via upload

---
+++ position.jsx
@@ -0,0 +1,20 @@
+import "./position.css"
+
+export function Position()
+{
+     return(
+        <div class="box">
+        <div class="box1">position - static</div>
+        <br></br>
+        <div class="box2">position - absolute</div>
+        <br></br>
+        <div class="box3">position - relative</div>
+        <br></br>
+        <div class="box4">position - sticky</div>
+        <br></br>
+        <div class="box5">position - fixed</div>
+    
+      
+        </div>
+     )
+}
